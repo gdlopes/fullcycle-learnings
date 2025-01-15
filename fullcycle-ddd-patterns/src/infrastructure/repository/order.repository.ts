9a8cee5190df5adb00433f21ac1d9ebfde8b3520
Gustavo Lopes <gustavolopes@Gustavos-MacBook-Air.local>
2025-01-15 07:28:31 -0300

    feat: implements find order method

--- fullcycle-ddd-patterns/src/infrastructure/repository/order.repository.ts
+++ fullcycle-ddd-patterns/src/infrastructure/repository/order.repository.ts
@@ -2,6 +2,7 @@ import Order from "../../domain/entity/order";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
 import OrderModel from "../db/sequelize/model/order.model";
 import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
+import OrderItem from "../../domain/entity/order_item";
 
 export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
@@ -36,7 +37,31 @@ export default class OrderRepository implements OrderRepositoryInterface {
   }
 
   async find(id: string): Promise<Order> {
-    throw new Error('Method not implemented')
+    try {
+      const foundOrder = await OrderModel.findOne({
+        where: {
+          id,
+        },
+        include: ["items"],
+        rejectOnEmpty: true,
+      });
+  
+      const orderItems = foundOrder.items.map(orderItem => {
+        const { id, name, price, product_id, quantity } = orderItem;
+        return new OrderItem(id, name, price, product_id, quantity);
+      })
+  
+      const order = new Order(
+        foundOrder.id,
+        foundOrder.customer_id,
+        orderItems,
+      );
+  
+      return order;
+    } catch(error) {
+      throw new Error('Fail to find the order')
+    }
+
   }
 
   async findAll(): Promise<Order[]> {
